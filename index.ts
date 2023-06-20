5bce07f4f7490ea862ae28552d2af569621546e0
Chaiwat Trisuwan <chaiwattsw@gmail.com>
2023-06-20 19:34:38 +0700

    feat: connect with supabase

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-// Import all dependencies, mostly using destructuring for better view.
 import "dotenv/config";
 import {
     ClientConfig,
@@ -10,15 +9,20 @@ import {
     MessageAPIResponseBase,
 } from "@line/bot-sdk";
 import express, { Application, Request, Response } from "express";
+import {
+    createClient,
+    SupabaseClient,
+    PostgrestResponse,
+} from "@supabase/supabase-js";
 
 // Setup all LINE client and Express configurations.
 const clientConfig: ClientConfig = {
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || "",
-    channelSecret: process.env.CHANNEL_SECRET,
+    channelSecret: process.env.CHANNEL_SECRET || "",
 };
 
 const middlewareConfig: MiddlewareConfig = {
-    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || "",
     channelSecret: process.env.CHANNEL_SECRET || "",
 };
 
@@ -30,7 +34,31 @@ const client = new Client(clientConfig);
 // Create a new Express application.
 const app: Application = express();
 
-// Function handler to receive the text.
+// Create a new Supabase client.
+const supabaseUrl = "YOUR_SUPABASE_URL";
+const supabaseKey = "YOUR_SUPABASE_KEY";
+const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
+
+// Function to fetch the passport data from Supabase.
+const fetchPassportData = async (): Promise<PostgrestResponse<any> | null> => {
+    try {
+        const { data, error } = await supabase
+            .from("passports")
+            .select("first_name, last_name, visa_date");
+
+        if (error) {
+            console.error(error);
+            return null;
+        }
+
+        return data;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+// Function handler to process the text event.
 const textEventHandler = async (
     event: WebhookEvent,
 ): Promise<MessageAPIResponseBase | undefined> => {
@@ -39,65 +67,61 @@ const textEventHandler = async (
         return;
     }
 
-    // Process all message related variables here.
-    const { replyToken } = event;
-    const { text } = event.message;
+    // Fetch the passport data from Supabase.
+    const passportData = await fetchPassportData();
 
-    // Create a new message.
-    const response: TextMessage = {
-        type: "text",
-        text,
-    };
+    // Check if passportData exists.
+    if (passportData) {
+        // Format the passport data into a string.
+        const passportList = passportData
+            .map((passport: any) => {
+                const { first_name, last_name, visa_date } = passport;
+                return `${first_name} ${last_name} - ${visa_date}`;
+            })
+            .join("\n");
 
-    // Reply to the user.
-    await client.replyMessage(replyToken, response);
+        // Create a response message.
+        const response: TextMessage = {
+            type: "text",
+            text: passportList,
+        };
+
+        // Reply to the user.
+        await client.replyMessage(event.replyToken, response);
+    }
 };
 
 // Register the LINE middleware.
-// As an alternative, you could also pass the middleware in the route handler, which is what is used here.
-// app.use(middleware(middlewareConfig));
+app.use(middleware(middlewareConfig));
 
 // Route handler to receive webhook events.
-// This route is used to receive connection tests.
-app.get("/", async (_: Request, res: Response): Promise<Response> => {
+app.post("/webhook", async (req: Request, res: Response): Promise<Response> => {
+    const events: WebhookEvent[] = req.body.events;
+
+    // Process all of the received events asynchronously.
+    const results = await Promise.all(
+        events.map(async (event: WebhookEvent) => {
+            try {
+                await textEventHandler(event);
+            } catch (err: unknown) {
+                if (err instanceof Error) {
+                    console.error(err);
+                }
+
+                // Return an error message.
+                return res.status(500).json({
+                    status: "error",
+                });
+            }
+        }),
+    );
+
+    // Return a successful message.
     return res.status(200).json({
         status: "success",
-        message: "Connected successfully!",
+        results,
     });
 });
-
-// This route is used for the Webhook.
-app.post(
-    "/webhook",
-    middleware(middlewareConfig),
-    async (req: Request, res: Response): Promise<Response> => {
-        const events: WebhookEvent[] = req.body.events;
-
-        // Process all of the received events asynchronously.
-        const results = await Promise.all(
-            events.map(async (event: WebhookEvent) => {
-                try {
-                    await textEventHandler(event);
-                } catch (err: unknown) {
-                    if (err instanceof Error) {
-                        console.error(err);
-                    }
-
-                    // Return an error message.
-                    return res.status(500).json({
-                        status: "error",
-                    });
-                }
-            }),
-        );
-
-        // Return a successfull message.
-        return res.status(200).json({
-            status: "success",
-            results,
-        });
-    },
-);
 
 // Create a server and listen to it.
 app.listen(PORT, () => {
